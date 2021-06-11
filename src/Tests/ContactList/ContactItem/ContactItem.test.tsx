0c85fc77a6bae98134e7c3870cbc542581bacd6c
Vadim Barkovsky <v.barkovsky@itransition.com>
2021-06-11 15:00:03 +0300

    fix bugs

--- src/Tests/ContactList/ContactItem/ContactItem.test.tsx
+++ src/Tests/ContactList/ContactItem/ContactItem.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import ContactItem from '../../../Components/ContactList/ContactItem';
 import store from '../../../Store';
--- src/Tests/ContactList/ContactList.test.tsx
+++ src/Tests/ContactList/ContactList.test.tsx
@@ -5,6 +5,22 @@ import ContactList from '../../Components/ContactList';
 import store from '../../Store';
 
 describe('ContactList test', () => {
+  const state = {
+    contacts: [{
+      "id": "001",
+      "image": "https://randomuser.me/api/portraits/med/men/73.jpg",
+      "fullImage": "https://randomuser.me/api/portraits/men/73.jpg",
+      "name": "Isaac",
+      "lastName": "Williams",
+      "login": "happykoala365",
+      "email": "isaac.williams@example.com",
+      "phone": "(058)-396-1659",
+      "rating": 0
+    }],
+    loading: false,
+    error: null,
+    category: null,
+  };
   let container;
 
   beforeEach(() => {
@@ -16,8 +32,7 @@ describe('ContactList test', () => {
   });
 
   test('should render ContactList component', () => {
-    const { getByTestId } = container;
-    const ContactListComponent = getByTestId('contactListComponent');
+    const ContactListComponent = container.getByTestId('contactListComponent');
     expect(ContactListComponent).toBeDefined();
   });
 });
